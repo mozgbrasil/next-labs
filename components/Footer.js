230fae8fe25051049b3b373e996a9aecef2fc825
mozgbrasil <mozgbrasil@gmail.com>
2021-08-30 22:05:54 -0300

    create-next-app

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        Made with{" "}
+        <img
+          src='/images/netliheart.svg'
+          alt='Netlify Logo'
+          className={styles.logo}
+        />{" "}
+        for you
+      </footer>
+    </>
+  );
+}
